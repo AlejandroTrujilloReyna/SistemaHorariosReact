956d37253b58223a078d0f7794a02caed1e9c7c5
AlejandroTrujilloReyna <alejandro.trujillo.reyna@uabc.edu.mx>
2024-04-18 11:24:16 -0700

    Creacion de la base

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'primereact/resources/themes/arya-green/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import '/node_modules/primeflex/primeflex.css'
+//import Axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"bdsistemahorario"
+});
+
+app.listen(3001,()=>{
+    console.log("Corriendo en el puerto 3001");
+});
