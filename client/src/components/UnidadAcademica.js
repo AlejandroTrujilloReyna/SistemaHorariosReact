9dea4ff9ffe98f327a09ebb68d8eec15137a4579
Leonard003tac <alvaradoaguilaleonardoalberto@gmail.com>
2024-04-27 20:54:47 -0700

    MODIFICACION UNIDAD ACADEMICA
    SE AGREGO LA VALIDACION DE LA MAXIMA LOGITUD EN EL CAMPO NOMBRE

--- client/src/components/UnidadAcademica.js
+++ client/src/components/UnidadAcademica.js
@@ -113,7 +113,7 @@ const UnidadAcademica = () => {
   };
   //EDITAR TEXTO
   const textEditor = (options) => {
-    return <InputText keyfilter={/[a-zA-Z\s]/} type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} onKeyDown={(e) => e.stopPropagation()} />;
+    return <InputText keyfilter={/[a-zA-Z\s]/} maxLength={255} type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} onKeyDown={(e) => e.stopPropagation()} />;
   };
   //COMPLETAR MODIFICACION
   const onCellEditComplete = (e) => {
