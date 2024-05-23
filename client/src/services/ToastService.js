127e6dd6c34c091749a6e411677b74ebb521838b
AlejandroTrujilloReyna <alejandro.trujillo.reyna@uabc.edu.mx>
2024-05-22 21:38:53 -0700

    Se agrego el archivo ToastService
    Se añadio el archivo ToastService con el proposito de no tener que declarar las variables toast en cada componente

---
+++ client/src/services/ToastService.js
@@ -0,0 +1,13 @@
+// src/services/toastService.js
+
+export const mostrarExito = (toast, mensaje) => {
+    toast.current.show({severity: 'success', summary: 'Exito', detail: mensaje, life: 3000});
+};
+
+export const mostrarAdvertencia = (toast, mensaje) => {
+    toast.current.show({severity: 'warn', summary: 'Advertencia', detail: mensaje, life: 3000});
+};
+
+export const mostrarError = (toast, mensaje) => {
+    toast.current.show({severity: 'error', summary: 'Error', detail: mensaje, life: 3000});
+};
