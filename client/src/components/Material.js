577521f55873ed9c482e22a116ed70fdab26a39c
LibradoPanduro <librado.panduro@uabc.edu.mx>
2024-06-06 08:28:49 -0700

    Modificación interfaz Grupo y Material

--- client/src/components/Material.js
+++ client/src/components/Material.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useState } from "react";
 import { useEffect } from "react";
-import { useRef } from "react";
-import { Panel } from 'primereact/panel';
+import { useRef } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Toast } from 'primereact/toast';
+import { mostrarExito, mostrarAdvertencia, mostrarError, mostrarInformacion } from '../services/ToastService';
+import { validarTexto} from '../services/ValidacionGlobalService';
+import { Toolbar } from 'primereact/toolbar';
+import { Dialog } from 'primereact/dialog';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import { FilterMatchMode } from 'primereact/api';
 import MaterialService from '../services/MaterialService';
 
 const Material = () => {
@@ -15,56 +22,73 @@ const Material = () => {
   const [nombre_Material,setnombre_Material] = useState("");
   //VARIABLES PARA LA CONSULTA
   const [materialList,setmaterialList] = useState([]);
-  const [filtroMaterial, setfiltroMaterial] = useState([]);
+  const [filtromaterial, setfiltromaterial] = useState([]);
+  const dt = useRef(null);
+  const [lazyState, setlazyState] = useState({
+    filters: {
+        nombre_Material: { value: '', matchMode: FilterMatchMode.STARTS_WITH }
+      },
+  });  
   //VARIABLE PARA LA MODIFICACION QUE INDICA QUE SE ESTA EN EL MODO EDICION
-  const [editando,seteditando] = useState(false);
+  const [datosCopia, setDatosCopia] = useState({
+    nombre_Material: "",
+  }); 
   //VARIABLES PARA EL ERROR
   const toast = useRef(null);
-  
-  //MENSAJE DE EXITO
-  const mostrarExito = (mensaje) => {
-    toast.current.show({severity:'success', summary: 'Exito', detail:mensaje, life: 3000});
-  }
-  //MENSAJE DE ADVERTENCIA
-  const mostrarAdvertencia = (mensaje) => {
-      toast.current.show({severity:'warn', summary: 'Advertencia', detail:mensaje, life: 3000});
-  }
-  //MENSAJE DE ERROR
-  const mostrarError = (mensaje) => {
-    toast.current.show({severity:'error', summary: 'Error', detail:mensaje, life: 3000});
-  }  
+  //ESTADOS PARA CONDICIONES
+  const [enviado, setEnviado] = useState(false);
+  const [abrirDialog,setAbrirDialog] = useState(0);  
+
+  const confirmar1 = (action) => {
+    confirmDialog({
+      message: '¿Seguro que quieres proceder?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sí',
+      rejectLabel: 'No',
+      defaultFocus: 'accept',
+      accept: action,
+      reject: () => mostrarAdvertencia(toast, "Cancelado")
+    });
+  };  
 
   //FUNCION PARA REGISTRAR
   const add = ()=>{
     //VALIDACION DE CAMPOS VACIOS
-    if (!nombre_Material) {      
-      mostrarAdvertencia("Existen campos Obligatorios vacíos");
+    if (!nombre_Material) {
+      mostrarAdvertencia(toast,"Existen campos Obligatorios vacíos");
+      setEnviado(true);
       return;
     }
+    const action = () => {
     //MANDAR A LLAMAR AL REGISTRO SERVICE
     MaterialService.registrarMaterial({
       nombre_Material:nombre_Material,
-    }).then(response=>{//CASO EXITOSO
-      if (response.status === 200) {
-        mostrarExito("Registro Exitoso");
+    }).then(response=>{
+      if (response.status === 200) {//CASO EXITOSO
+        mostrarExito(toast,"Registro Exitoso");
         get();
         limpiarCampos();
+        setEnviado(false);
+        setAbrirDialog(0);
       }
     }).catch(error=>{//EXCEPCIONES
-      if (error.response.status === 400) {        
-        mostrarAdvertencia("Clave ya Existente");
-      }else if(error.response.status === 401){
-        mostrarAdvertencia("Nombre ya Existente");        
-      }else if(error.response.status === 500){  
-        mostrarError("Error interno del servidor");
+      if (error.response.status === 400) {
+        mostrarAdvertencia(toast,"Clave ya Existente");
+      } else if (error.response.status === 401) {
+        mostrarAdvertencia(toast,"Nombre ya Existente");      
+      }else if(error.response.status === 500){          
+        mostrarError(toast,"Error interno del servidor");
       }     
-    });
+    });    
+    };
+    confirmar1(action);
   }  
 
   //FUNCION PARA CONSULTA
   const get = ()=>{
     MaterialService.consultarMaterial().then((response)=>{//CASO EXITOSO
-      setmaterialList(response.data);      
+      setmaterialList(response.data);  
     }).catch(error=>{//EXCEPCIONES
       if (error.response.status === 500) {
       }
@@ -72,132 +96,175 @@ const Material = () => {
   }
 
   //FUNCION PARA LA MODIFICACION
-  const put = (rowData) =>{
-    MaterialService.modificarMaterial(rowData).then((response)=>{//CASO EXITOSO
-      if (response.status === 200) {
-        mostrarExito("Modificación Exitosa");        
+  const put = () =>{
+  if (!nombre_Material) {
+    mostrarAdvertencia(toast,"Existen campos Obligatorios vacíos");
+    setEnviado(true);
+    return;
+  }
+  if (nombre_Material === datosCopia.nombre_Material) {
+    mostrarInformacion(toast, "No se han realizado cambios");
+    setAbrirDialog(0);
+    limpiarCampos();
+    return;
+  }
+  const action = () => {  
+  MaterialService.modificarMaterial({
+    nombre_Material:nombre_Material,
+    }).then(response=>{//CASO EXITOSO
+      if(response.status === 200){
+        mostrarExito(toast, "Modificación Exitosa");
+        get();
+        limpiarCampos();
+        setEnviado(false);
+        setAbrirDialog(0);
       }
     }).catch(error=>{//EXCEPCIONES
-      if (error.response.status === 401) {
-        mostrarAdvertencia("El Nombre ya se encuentra Registrado");
+      if(error.response.status === 401){
+        mostrarAdvertencia(toast,"Nombre ya Existente");
         get();
-      }else if (error.response.status === 500) {
-        mostrarError("Error del sistema");
+      }else if(error.response.status === 500){
+        mostrarError(toast,"Error del sistema");
       }
-    });
+    })
+  };
+  confirmar1(action);    
   }
 
   //!!!EXTRAS DE REGISTRO
 
   //FUNCION PARA LIMPIAR CAMPOS AL REGISTRAR
   const limpiarCampos = () =>{
-    setnombre_Material("");    
-  } 
-
-  //!!!EXTRAS DE CONSULTA
+    setnombre_Material("");
+  }
   
+  //!!!EXTRAS DE CONSULTA
+
   //COLUMNAS PARA LA TABLA
   const columns = [
-    {field: 'clave_Material', header: 'Clave' },
-    {field: 'nombre_Material', header: 'Nombre' },
+    {field: 'clave_Material', header: 'Clave', filterHeader: 'Filtro por Clave' },
+    {field: 'nombre_Material', header: 'Nombre', filterHeader: 'Filtro por Nombre' },    
   ];
   
   //MANDAR A LLAMAR A LOS DATOS EN CUANTO SE INGRESA A LA PAGINA
   useEffect(() => {
     get();
-  },[]);
-  
+  }, []);
+
   //FUNCION PARA LA BARRA DE BUSQUEDA
   const onSearch = (e) => {
     const value = e.target.value.toLowerCase();
     const filteredData = materialList.filter((item) => {
         return (
-            item.clave_Material.toString().includes(value) ||
-            item.nombre_Material.toLowerCase().includes(value)
+            item.nombre_Material.toLowerCase().includes(value)          
         );
     });
-    setfiltroMaterial(filteredData);
+    setfiltromaterial(filteredData);
+  };
+  
+  //BOTON PARA MODIFICAR
+  const accionesTabla = (rowData) => {
+    return (<>
+        <Button
+          icon="pi pi-pencil"
+          rounded
+          outlined
+          className="m-1"
+          onClick={() => {
+            setnombre_Material(rowData.nombre_Material);
+            setDatosCopia({
+                nombre_Material: rowData.nombre_Material
+            });
+            setAbrirDialog(2);
+          }}          
+        />     
+        </>
+    );
+  };    
+
+  //FUNCION PARA QUE SE MUESTRE INFORMACION ESPECIFICA DE LAS LLAVES FORANEAS
+  const renderBody = (rowData, field) => {
+      return rowData[field]; 
+  };
+  
+  //ENCABEZADO DEL DIALOG
+  const headerTemplate = (
+    <div className="formgrid grid justify-content-center border-bottom-1 border-300">
+      {abrirDialog===1 && (<h4>Registrar Material</h4>)}
+      {abrirDialog===2 && (<h4>Modificar Material</h4>)}
+    </div>
+  );
+  
+  //LISTA DE OPCIONES DE HERRAMIENTAS
+  const Herramientas = () => {
+    return (<div className="flex justify-content-between flex-wrap gap-2 align-items-center">
+            <Button label="Nuevo" icon="pi pi-plus" severity="success" onClick={()=>setAbrirDialog(1)}/>
+            <Button label="Exportar" icon="pi pi-upload" className="p-button-help"  onClick={()=>{dt.current.exportCSV();}}/>
+              <IconField iconPosition="left">
+                <InputIcon className="pi pi-search" />
+                <InputText type="search" placeholder="Buscar..." maxLength={255} onChange={onSearch}/>  
+              </IconField>
+            </div>              
+    );
+  };
+  
+  //FUNCION PARA ACTIVAR EL FILTRADO
+  const onFilter = (event) => {
+    event['first'] = 0;
+    setlazyState(event);
   };  
-  
-  //ACTIVAR EDICION DE CELDA
-  const cellEditor = (options) => {
-    switch(options.field){
-      case 'nombre_Material':
-        return textEditor(options);     
-        default:
-          return;   
-    }    
-  };
-
-  //EDITAR TEXTO
-  const textEditor = (options) => {
-    return <InputText type="text" keyfilter={/[a-zA-ZñÑ\s]/} value={options.value} maxLength={255} 
-    onChange={(e) => { 
-      if (validarTexto(e.target.value)) { 
-        options.editorCallback(e.target.value)
-      }
-    }}
-    onKeyDown={(e) => e.stopPropagation()} />;
-  };
-  
-  //COMPLETAR MODIFICACION
-  const onCellEditComplete = (e) => {            
-      let { rowData, newValue, field, originalEvent: event } = e;                          
-      switch (field) {
-        case 'nombre_Material':
-          if (newValue.trim().length > 0 && newValue !== rowData[field]){                                    
-                rowData[field] = newValue;               
-                put(rowData);                       
-          }else{                             
-            event.preventDefault();
-          } 
-          break;
-        default:
-          break;
-      }
-      seteditando(false);
-  }; 
-
-  const validarTexto = (value) => {
-    const regex = /^[a-zA-Z\s]*$/;
-    return regex.test(value);
-  };
 
   return (
     <>
+    {/*APARICION DE LOS MENSAJES (TOAST)*/}
     <Toast ref={toast} />
+    <Toolbar start={<h2 className="m-0">Grupo</h2>} end={Herramientas}/>
+    <ConfirmDialog />
       {/*PANEL PARA EL REGISTRO*/}
-      <Panel header="Registrar Material" className='mt-3' toggleable>        
-        <div className="formgrid grid mx-8">
-          <div className="field col-10">
+      <Dialog className='w-7' header={headerTemplate} closable={false} visible={abrirDialog!==0} onHide={() => {setAbrirDialog(0)}}>
+        <div className="formgrid grid justify-content-center">
+          <div className="field col-4">
               <label>Nombre*</label>
-              <InputText type="text" keyfilter={/[a-zA-ZñÑ\s]/} value={nombre_Material} maxLength={255}
+              <InputText invalid={enviado===true && !nombre_Material} type="text" keyfilter={/^[a-zA-Z\s]+$/} value={nombre_Material} maxLength={255}
                   onChange={(event)=>{
-                    if (validarTexto(event.target.value)) {
+                    if (validarTexto(event.target.value)) {  
                       setnombre_Material(event.target.value);
                     }
                   }}  
-                  placeholder="Ej.Laboratorio de LSC" 
-              className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full custom-input-text"/>              
-          </div>                                                                            
+                  placeholder="Ej.Cañon" 
+              className="w-full"/>              
+          </div>                                                                        
         </div>
-        <div className="mx-8 mt-4">
-          <Button label="Guardar" onClick={add} className="p-button-success" />
-        </div>                
-      </Panel>
-      {/*PANEL PARA LA CONSULTA DONDE SE INCLUYE LA MODIFICACION*/}
-      <Panel header="Consultar Edificios" className='mt-3' toggleable>
-        <div className="mx-8 mb-4">
-          <InputText type="search" placeholder="Buscar..." maxLength={255} onChange={onSearch} className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none w-full" />  
-        </div>
-        <DataTable value={filtroMaterial.length ? filtroMaterial :materialList} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} editMode='cell' size='small' tableStyle={{ minWidth: '50rem' }}>
-          {columns.map(({ field, header }) => {
-              return <Column sortable={editando === false} key={field} field={field} header={header} style={{ width: '25%' }} editor={field === 'clave_Material' ? null : (options) => cellEditor(options)}
-              onCellEditComplete={onCellEditComplete}
-              onCellEditInit={(e) => seteditando(true)}/>;
+        <div className="formgrid grid justify-content-end">
+          <Button label="Cancelar" icon="pi pi-times" outlined className='m-2' onClick={() => {setAbrirDialog(0); setEnviado(false); limpiarCampos();}} severity='secondary' />
+          {abrirDialog===1 && (
+            <Button label="Guardar" icon="pi pi-check" className='m-2' onClick={add} severity='success' />
+          )}
+          {abrirDialog===2 && (
+            <Button label="Editar" icon="pi pi-check" className='m-2' onClick={put} severity='success' />
+          )}          
+        </div> 
+      </Dialog>  
+        <DataTable 
+        onFilter={onFilter} filters={lazyState.filters} filterDisplay="row" 
+        scrollable scrollHeight="78vh"
+        ref={dt}         
+        value={filtromaterial.length ? filtromaterial :materialList} 
+        size='small'>
+          {columns.map(({ field, header, filterHeader }) => {
+              return <Column style={{minWidth:'40vh'}} bodyStyle={{textAlign:'center'}} sortable filter filterPlaceholder={filterHeader}
+              filterMatchModeOptions={[
+                { label: 'Comienza con', value: FilterMatchMode.STARTS_WITH },
+                { label: 'Contiene', value: FilterMatchMode.CONTAINS },
+                { label: 'No contiene', value: FilterMatchMode.NOT_CONTAINS },
+                { label: 'Termina con', value: FilterMatchMode.ENDS_WITH },
+                { label: 'Igual', value: FilterMatchMode.EQUALS },
+                { label: 'No igual', value: FilterMatchMode.NOT_EQUALS },
+              ]} 
+              key={field} field={field} header={header} body={(rowData) => renderBody(rowData, field)}/>;
           })}
-        </DataTable>          
-      </Panel>     
+          <Column body={accionesTabla} alignFrozen={'right'} frozen={true}></Column>    
+        </DataTable>  
     </>
   )
 }
